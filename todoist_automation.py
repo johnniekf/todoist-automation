109a8762993d7c35a33dbdb1c0144547fbf62a30
johnniekf <johnnie.kf@gmail.com>
2023-06-24 15:09:13 +0100

    Todoist implemented with YT api, now posts video of a single youtuber.
    ToDo:
    
    Add checking to stop the same video being posted more than once.
    Add ability for multiple youtubers to be searched and posted seperately.
    Add google sheets functionality to allow faster integration (or todoist method).

--- todoist_automation.py
+++ todoist_automation.py
@@ -1,28 +1,46 @@
+import os
 import json
 from todoist_api_python.api import TodoistAPI
+import googleapiclient.discovery
+import googleapiclient.errors
 
-# TODO: Add subhub to allow instant posting to Todoist
-# Allow google sheets integration
-# 
+def main():
+    api_service_name = "youtube"
+    api_version = "v3"
 
-
-
-with open('secret.json') as f:
-		data = json.load(f)
-api = data['todoist_api']
-
-# Pull data from Todoist
-# try:
-#     projects = api.get_projects()
-#     print(projects)
-# except Exception as error:
-#     print(error)
-
-try:
-    task = api.add_task(content="Test Task (with label)", labels={"Youtube"})
-    print(task)
-except Exception as error:
-    print(error)
+    # Load the API key from a JSON file
+    with open('secret.json') as f:
+        data = json.load(f)
+    yt_key = data['yt_api']
+    tdist_key = data['todoist_api']
     
+    api = TodoistAPI(tdist_key)
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=yt_key)
+
+    request = youtube.search().list(
+        part="snippet",
+        channelId="UCsBjURrPoezykLs9EqgamOA",
+        maxResults=4,
+        order="date"
+    )
     
-exit()
+    response = request.execute()
+    
+        # Print the title and URL of each video
+    for item in response['items']:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        url = f'https://www.youtube.com/watch?v={video_id}'
+        print(f'Title: {title}')
+        print(f'URL: {url}')
+        # [This text will be hyperlinked](http://todoist.com/)
+    try:
+        task = api.add_task(content=f'[{title}]({url})')
+        print(task)
+    except Exception as error:
+        print(error)
+        
+if __name__ == "__main__":
+    main()
