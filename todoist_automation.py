3b608e95e1e7cb5f7a82a34470f6971844a1620f
johnniekf <johnnie.kf@gmail.com>
2023-06-27 11:17:45 +0100

    Added multi-search and 24hr upload window

--- todoist_automation.py
+++ todoist_automation.py
@@ -1,46 +1,99 @@
 import os
+import datetime
 import json
 from todoist_api_python.api import TodoistAPI
 import googleapiclient.discovery
 import googleapiclient.errors
 
-def main():
+# Load the API keys from a JSON file
+with open('secret.json') as f:
+    data = json.load(f)
+yt_key = data['yt_api']
+tdist_key = data['todoist_api']
+sheet_id = data['sheet_id']
+
+# TODO: Check cells for data and add to an array
+# Define the range of cells to read
+range_name = 'Sheet1!A1:A'
+
+# Read the values of the cells in the range
+service = googleapiclient.discovery.build('sheets', 'v4', developerKey=yt_key)
+result = service.spreadsheets().values().get(
+    spreadsheetId=sheet_id, range=range_name).execute()
+values = result.get('values', [])
+
+# # Define an empty array to store the IDs
+# ids = []
+
+# # Loop through the values and add any non-blank cells to the array
+# for row in values:
+#     if row:
+#         ids.append(row[0])
+#     else:
+#         break
+
+# # Print the array of IDs
+# print(ids)
+
+# Get the channel ID from the cell value
+# channel_id = cell_value.strip()
+# print(channel_id)
+
+def main(channel_id):
     api_service_name = "youtube"
     api_version = "v3"
-
-    # Load the API key from a JSON file
-    with open('secret.json') as f:
-        data = json.load(f)
-    yt_key = data['yt_api']
-    tdist_key = data['todoist_api']
     
     api = TodoistAPI(tdist_key)
 
+    # Get the latest videos from the YouTube channel
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=yt_key)
+    
+    # Define the date and time 24 hours ago
+    now = datetime.datetime.utcnow()
+    day_ago = now - datetime.timedelta(days=1)
+    
+    day_ago_str = day_ago.isoformat() + 'Z'
 
+    # Todo: Add multi-search and get video IDs from Google Sheets
     request = youtube.search().list(
         part="snippet",
-        channelId="UCsBjURrPoezykLs9EqgamOA",
-        maxResults=4,
-        order="date"
+        channelId=channel_id,
+        maxResults=10,
+        order="date",
+        publishedAfter=day_ago_str
     )
     
+    # Get the response from the YouTube API
     response = request.execute()
     
-        # Print the title and URL of each video
+    # Todo: Add a check to see if the video has already been added to Todoist
+    # Extract the title and URL of each video from the response
     for item in response['items']:
         title = item['snippet']['title']
         video_id = item['id']['videoId']
+        author = item['snippet']['channelTitle']
         url = f'https://www.youtube.com/watch?v={video_id}'
-        print(f'Title: {title}')
-        print(f'URL: {url}')
-        # [This text will be hyperlinked](http://todoist.com/)
-    try:
-        task = api.add_task(content=f'[{title}]({url})')
-        print(task)
-    except Exception as error:
-        print(error)
+        try:
+            task = api.add_task(content=f'[{title}]({url})', labels=[f'{author}'])
+            print(author + " - " + title)
+        except Exception as error:
+            print(error)
         
-if __name__ == "__main__":
-    main()
+
+# Define an empty array to store the IDs
+ids = []
+
+# Loop through the values and add any non-blank cells to the array
+for row in values:
+    if row:
+        ids.append(row[0])
+    else:
+        break
+
+# print(ids)
+
+# TODO: Add as a loop depending on sheets data
+for id in ids:
+    main(id)
+    # print(id)
